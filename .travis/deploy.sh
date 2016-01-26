50ef70bf75da4fcd3a021796d80fbe74cb6bbf29
Martin Giger <martin@humanoids.be>
2016-01-26 19:16:13 +0100

    Try to leak env

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 set -x
-$TRAVIS_BUILD_DIR/.travis/trust-server.sh
+. $TRAVIS_BUILD_DIR/.travis/trust-server.sh
 ssh humanoid@humanoids.be 'cd www/twitchbots && git pull && rm -rf cache && php composer.phar install --no-dev --optimize-autoloader'
