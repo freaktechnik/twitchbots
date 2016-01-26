be4bb4d93f53192f91f94c65365497fcd1095e3b
Martin Giger <martin@humanoids.be>
2016-01-26 12:22:18 +0100

    Do composer installing

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,3 @@
 #! /bin/sh
-eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_a666911b0f49_key -iv $encrypted_a666911b0f49_iv -in .travis/id_rsa.enc -out .travis/id_rsa -d
-chmod 600 .travis/id_rsa
-ssh-add .travis/id_rsa
-ssh-keyscan -H humanoids.be >> ~/.ssh/known_hosts
-# TODO update vendor deps
-ssh humanoid@humanoids.be 'cd www/twitchbots && git pull && rm -rf cache'
+./trust-server.sh
+ssh humanoid@humanoids.be 'cd www/twitchbots && git pull && rm -rf cache && php composer.phar install --no-dev --optimize-autoloader'
---
+++ .travis/trust-server.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_a666911b0f49_key -iv $encrypted_a666911b0f49_iv -in .travis/id_rsa.enc -out .travis/id_rsa -d
+chmod 600 .travis/id_rsa
+ssh-add .travis/id_rsa
+ssh-keyscan -H humanoids.be >> ~/.ssh/known_hosts
