70e8a4f6ca7bce5101fa6b127fc2bc1883bb9c49
Martin Giger <martin@humanoids.be>
2016-01-26 19:28:41 +0100

    whatevs

--- .travis/trust-server.sh
+++ .travis/trust-server.sh
@@ -4,4 +4,4 @@ eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_a666911b0f49_key -iv $encrypted_a666911b0f49_iv -in $TRAVIS_BUILD_DIR/.travis/id_rsa.enc -out $TRAVIS_BUILD_DIR/.travis/id_rsa -d
 chmod 600 $TRAVIS_BUILD_DIR/.travis/id_rsa
 ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa
-ssh-keyscan -H humanoids.be >> ~/.ssh/known_hosts
+ssh-keyscan -H humanoids.be >> ~/.ssh/known_hosts   
