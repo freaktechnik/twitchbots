dae74359ee4791b29d39b4e3217b19f8359c960e
Martin Giger <martin@humanoids.be>
2016-01-26 18:49:28 +0100

    Use the correct env var

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$HOME/.travis/trust-server.sh
+$TRAVIS_BUILD_DIR/.travis/trust-server.sh
 ssh humanoid@humanoids.be 'cd www/twitchbots && git pull && rm -rf cache && php composer.phar install --no-dev --optimize-autoloader'
--- .travis/trust-server.sh
+++ .travis/trust-server.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_a666911b0f49_key -iv $encrypted_a666911b0f49_iv -in $HOME/.travis/id_rsa.enc -out $HOME/.travis/id_rsa -d
-chmod 600 $HOME/.travis/id_rsa
-ssh-add $HOME/.travis/id_rsa
+openssl aes-256-cbc -K $encrypted_a666911b0f49_key -iv $encrypted_a666911b0f49_iv -in $TRAVIS_BUILD_DIR/.travis/id_rsa.enc -out $TRAVIS_BUILD_DIR/.travis/id_rsa -d
+chmod 600 $TRAVIS_BUILD_DIR/.travis/id_rsa
+ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa
 ssh-keyscan -H humanoids.be >> ~/.ssh/known_hosts
