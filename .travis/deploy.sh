59a0507886c1b532180179cfb2b7f3bd0a98ee30
Martin Giger <martin@humanoids.be>
2016-01-26 19:33:50 +0100

    Use php7

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 set -x
 source $TRAVIS_BUILD_DIR/.travis/trust-server.sh
-ssh humanoid@humanoids.be 'cd www/twitchbots && git pull && rm -rf cache && php composer.phar install --no-dev --optimize-autoloader'
+ssh humanoid@humanoids.be 'cd www/twitchbots && git pull && rm -rf cache && php70 composer.phar install --no-dev --optimize-autoloader'
