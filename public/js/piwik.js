e5c5c652c45d98797fa8ae5963e62427e4033651
Martin Giger <martin@humanoids.be>
2016-02-25 23:27:03 +0100

    Make that all fit into a CSP

---
+++ public/js/piwik.js
@@ -0,0 +1,11 @@
+var _paq = _paq || [];
+_paq.push(["setDomains", ["*.twitchbots.info"]]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u="//humanoids.be/stats/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 5]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
