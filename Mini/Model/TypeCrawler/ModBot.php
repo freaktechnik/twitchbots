6060c61f4bf5f0066a541d72e21e9840cd4158f4
Martin Giger <martin@humanoids.be>
2016-05-14 01:19:57 +0200

    properly declare modbot

--- Mini/Model/TypeCrawler/ModBot.php
+++ Mini/Model/TypeCrawler/ModBot.php
@@ -11,7 +11,7 @@ class ModBot extends TypeCrawler {
     /** @var int */
     public static $type = 28;
 
-    __construct(TypeCrawlerStorage $storage) {
+    function __construct(TypeCrawlerStorage $storage) {
         parent::__construct($storage);
     }
 
