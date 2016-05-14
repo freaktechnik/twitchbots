777d0670cd99ddb0d617a74211f41ead444e5483
Martin Giger <martin@humanoids.be>
2016-05-14 11:46:03 +0200

    Yay, broken thing

--- Mini/console.php
+++ Mini/console.php
@@ -24,7 +24,7 @@ $console
     ->setDefinition(array(
         new InputOption('amount', 'a', InputOption::VALUE_OPTIONAL, 'Number of bots to check', 10)
     ))
-    ->setD('Run all check tasks')
+    ->setDescription('Run all check tasks')
     ->setCode(function (InputInterface $input, OutputInterface $output) use ($model, $console) {
         if($model->checkRunning()) {
             $date = '['.date('r').'] ';
