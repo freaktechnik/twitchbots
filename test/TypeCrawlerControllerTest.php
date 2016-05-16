7ca015510a9aba280af7b88ff25f27b5683b5645
Martin Giger <martin@humanoids.be>
2016-05-17 00:33:49 +0200

    Fix class namespacing?

--- test/TypeCrawlerControllerTest.php
+++ test/TypeCrawlerControllerTest.php
@@ -4,11 +4,11 @@ class TypeCrawlerControllerTest extends PHPUnit_Framework_TestCase
 {
     public function testCrawl()
     {
-        $storage = new \Mini\Model\TypeCrawler\Storage\StorageFactory('ShimTypeCrawlerStorage', array());
+        $storage = new \Mini\Model\TypeCrawler\Storage\StorageFactory('\ShimTypeCrawlerStorage', array());
         $controller = new \Mini\Model\TypeCrawler\TypeCrawlerController($storage);
-        $controller->registerCrawler('ShimTypeCrawler');
+        $controller->registerCrawler('\ShimTypeCrawler');
 
-        $crawled = $controller->crawl('ShimTypeCrawler');
+        $crawled = $controller->crawl('\ShimTypeCrawler');
         $this->assertCount(1, $crawled);
         $this->assertInstanceOf('\\stdClass', $crawled[0]);
         $this->assertEquals('test', $crawled[0]->name);
