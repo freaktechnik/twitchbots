4f438dfb6055d16d279395b40f70dbf68436a44a
Martin Giger <martin@humanoids.be>
2016-06-19 10:23:03 +0200

    This needs to be 4 derp

--- test/ModelTest.php
+++ test/ModelTest.php
@@ -228,7 +228,7 @@ class ModelTest extends PHPUnit_Extensions_Database_TestCase
         $this->httpMock->append(new Response(200));
         $this->model->addCorrection("ncsmusicbot", 22, "", "ncsmusic");
 
-        $this->assertEquals(3, $this->getConnection()->getRowCount('submissions'), "Adding correction failed");
+        $this->assertEquals(4, $this->getConnection()->getRowCount('submissions'), "Adding correction failed");
 
         $queryTable = $this->getConnection()->createQueryTable(
            'submissions',
