77dc4517240de984564ed3c5a5ae805fbe057588
Martin Giger <martin@humanoids.be>
2016-07-27 16:52:59 +0200

    derp

---
+++ Mini/Model/PingablePDO.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mini\Model;
+
+class PingablePDO {
+    private $pdo;
+    private $params;
+
+    public function __construct()
+    {
+        $this->params = func_get_args();
+        $this->init();
+    }
+
+    public function __call(string $name, array $args)
+    {
+        return call_user_func_array(array($this->pdo, $name), $args);
+    }
+
+    public function ping()
+    {
+        try {
+            $this->pdo->query("DO 1");
+        } catch(Exception $e) {
+            $this->init();
+        }
+    }
+
+    public function init()
+    {
+        $class = new ReflectionClass('PDO');
+        $this->pdo = $class->newInstanceArgs($this->params);
+    }
+}
+
