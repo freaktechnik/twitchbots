ccde98626ae88d4d3eb9eb357535b38d86ad26a1
Martin Giger <martin@humanoids.be>
2016-11-09 22:43:57 +0100

    Remove deprecated CSP directive

--- public/index.php
+++ public/index.php
@@ -73,7 +73,7 @@ $app->configureMode('production', function () use ($app) {
             'db_pass' => $db_pw,
             'page_size' => 50
         ),
-        'csp' => "default-src 'none'; style-src 'self'; script-src 'self' https://humanoids.be; font-src 'self'; connect-src https://api.twitchbots.info https://api.twitch.tv; form-action 'self'; frame-ancestors 'none'; reflected-xss block; base-uri twitchbots.info www.twitchbots.info; referrer no-referrer-when-downgrade; child-src https://humanoids.be; frame-src https://humanoids.be; img-src https://humanoids.be 'self'",
+        'csp' => "default-src 'none'; style-src 'self'; script-src 'self' https://humanoids.be; font-src 'self'; connect-src https://api.twitchbots.info https://api.twitch.tv; form-action 'self'; frame-ancestors 'none'; reflected-xss block; base-uri twitchbots.info www.twitchbots.info; child-src https://humanoids.be; frame-src https://humanoids.be; img-src https://humanoids.be 'self'",
         'apiUrl' => $app->request->getScheme().'://api.'.$app->request->getHost(),
         'canonicalUrl' => 'https://'.$app->request->getHost().'/'
     ));
