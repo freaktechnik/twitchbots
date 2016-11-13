b712f38bcd2b459200ed2159c8d19ec5a60c3131
Martin Giger <martin@humanoids.be>
2016-11-13 17:03:10 +0100

    actually

--- Mini/Model/Submissions.php
+++ Mini/Model/Submissions.php
@@ -38,7 +38,7 @@ class Submissions extends PaginatingStore {
 
     public function getSubmissions(): array
     {
-        $query = $this->prepareSelect("*, (IFNULL(offline,0) + IFNULL(ismod,0) + COALESCE(following<10,0) - IFNULL(vods,1) - (description REGEXP '^[^0-9].*$') - (2 - IFNULL(online+online,0))) AS score", "ORDER BY date ASC, score DESC, online DESC");
+        $query = $this->prepareSelect("*, (IFNULL(offline,0) + IFNULL(ismod,0) + COALESCE(following<10,0) - IFNULL(vods,1) - (description REGEXP '^[^0-9].*$') - (2 - IFNULL(online+online,0))) AS score", "ORDER BY score DESC, online DESC, date DESC");
         $query->execute();
 
         return $query->fetchAll();
