220393ca852045f95be29fa8e75cad1ced985dc2
Martin Giger <martin@humanoids.be>
2017-09-03 23:02:50 +0200

    Select hashed tab on select

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -3,4 +3,7 @@ require(['bootstrap', 'jquery'], function() {
         e.preventDefault();
         $(this).tab('show');
     });
+    if(navigator.location.hash) {
+        $('.nav-tabs a[href="' + navigator.location.hash + '"]').tab('show');
+    }
 });
