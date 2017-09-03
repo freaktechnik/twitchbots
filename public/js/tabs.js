6407385766af6cae71d3b2050648b30696d4362b
Martin Giger <martin@humanoids.be>
2017-09-03 23:10:51 +0200

    try tarr version

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -2,7 +2,7 @@ require(['bootstrap', 'jquery'], function() {
     $('.nav-tabs a').click(function(e) {
         e.preventDefault();
         $(this).tab('show');
-        window.location.hash = $(this).prop('href');
+        window.location.hash = $(this).attr('href');
     });
     if(window.location.hash) {
         $('.nav-tabs a[href="' + window.location.hash + '"]').tab('show');
