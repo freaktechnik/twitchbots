d68295043326f4d33a225aceab614a4968948eec
Martin Giger <martin@humanoids.be>
2017-09-03 23:07:50 +0200

    Still update the has

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -2,6 +2,7 @@ require(['bootstrap', 'jquery'], function() {
     $('.nav-tabs a').click(function(e) {
         e.preventDefault();
         $(this).tab('show');
+        window.location.hash = $(this).prop('href');
     });
     if(window.location.hash) {
         $('.nav-tabs a[href="' + window.location.hash + '"]').tab('show');
