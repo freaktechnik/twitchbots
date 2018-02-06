15980bead0d131ceb5a1ccbada141dde83e86446
Martin Giger <martin@humanoids.be>
2018-02-07 00:09:42 +0100

    ci: remove dbugging

--- .travis/trust-server.sh
+++ .travis/trust-server.sh
@@ -4,4 +4,3 @@ openssl aes-256-cbc -K $encrypted_a666911b0f49_key -iv $encrypted_a666911b0f49_i
 chmod 600 $TRAVIS_BUILD_DIR/.travis/id_rsa
 ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa
 echo "$DEPLOY_HOST $SERVER_PUBLIC_KEY" >> $HOME/.ssh/known_hosts
-cat $HOME/.ssh/known_hosts
