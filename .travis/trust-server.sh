d67475f2bd5b43d45cb32bd522470dc751acdbeb
Martin Giger <martin@humanoids.be>
2018-02-06 23:50:30 +0100

    fix: obtain server ssh signature out-of-band

--- .travis/trust-server.sh
+++ .travis/trust-server.sh
@@ -3,4 +3,4 @@ eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_a666911b0f49_key -iv $encrypted_a666911b0f49_iv -in $TRAVIS_BUILD_DIR/.travis/id_rsa.enc -out $TRAVIS_BUILD_DIR/.travis/id_rsa -d
 chmod 600 $TRAVIS_BUILD_DIR/.travis/id_rsa
 ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa
-ssh-keyscan -H humanoids.be >> ~/.ssh/known_hosts   
+echo $SERVER_PUBLIC_KEY >> ~/.ssh/known_hosts   
