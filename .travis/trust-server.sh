9858535061e7512dab0d388e062a9b971551666c
Martin Giger <martin@humanoids.be>
2018-02-07 00:01:32 +0100

    ci: debug

--- .travis/trust-server.sh
+++ .travis/trust-server.sh
@@ -3,4 +3,5 @@ eval "$(ssh-agent -s)"
 openssl aes-256-cbc -K $encrypted_a666911b0f49_key -iv $encrypted_a666911b0f49_iv -in $TRAVIS_BUILD_DIR/.travis/id_rsa.enc -out $TRAVIS_BUILD_DIR/.travis/id_rsa -d
 chmod 600 $TRAVIS_BUILD_DIR/.travis/id_rsa
 ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa
-echo "$SERVER_PUBLIC_KEY" >> $HOME/.ssh/known_hosts
+echo '$SERVER_PUBLIC_KEY' >> $HOME/.ssh/known_hosts
+cat $HOME/.ssh/known_hosts
