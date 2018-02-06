d2db43c1cf7c1a0f49b9887e8cf094a3ddcbc0b2
Martin Giger <martin@humanoids.be>
2018-02-06 23:53:43 +0100

    fix: parametrize ssh connection credentials

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 set -x
 source $TRAVIS_BUILD_DIR/.travis/trust-server.sh
-ssh humanoid@humanoids.be 'cd www/twitchbots && git pull && rm -rf cache && php70 composer.phar install --no-dev --optimize-autoloader'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'cd www/twitchbots && git pull && rm -rf cache && php70 composer.phar install --no-dev --optimize-autoloader'
