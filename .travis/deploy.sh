a2d493fa183ae023941492f996afb51f02ba83e8
Martin Giger <martin@humanoids.be>
2018-06-04 20:18:07 +0200

    tests: use php 7.1 to install composer deps

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 set -x
 source $TRAVIS_BUILD_DIR/.travis/trust-server.sh
-ssh $DEPLOY_USER@$DEPLOY_HOST 'cd www/twitchbots && git pull && rm -rf cache && php70 composer.phar install --no-dev --optimize-autoloader'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'cd www/twitchbots && git pull && rm -rf cache && php71 composer.phar install --no-dev --optimize-autoloader'
