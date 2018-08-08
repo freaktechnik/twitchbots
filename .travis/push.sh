b1c40f52c64ca57f41cab8dbb82f0bb3520b4f98
Martin Giger <martin@humanoids.be>
2018-08-08 22:07:19 +0200

    ci: fix push script

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,11 +1,9 @@
 #! /bin/sh
-
-if [ -z ${DEPLOY_HOST + x} ]
+if [ -z ${DEPLOY_HOST+x} ]
 then
     echo "No deploy config found"
     exit 1
 fi
-
 deploy () {
     ssh $DEPLOY_USER@$DEPLOY_HOST "cd www/$1 && git pull && rm -rf cache && php71 composer.phar install --no-dev --optimize-autoloader"
 }
