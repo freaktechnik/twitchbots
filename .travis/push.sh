bff9612ce80cb292e531f3eab73a2b4dc96eb7c2
Martin Giger <martin@humanoids.be>
2020-07-04 02:32:18 +0200

    ci: deploy with newer php

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,5 +5,5 @@ then
     exit 1
 fi
 deploy () {
-    ssh $DEPLOY_USER@$DEPLOY_HOST "cd www/$1 && git pull && rm -rf cache && php71 composer.phar install --no-dev --optimize-autoloader"
+    ssh $DEPLOY_USER@$DEPLOY_HOST "cd www/$1 && git pull && rm -rf cache && php74 composer.phar install --no-dev --optimize-autoloader"
 }
